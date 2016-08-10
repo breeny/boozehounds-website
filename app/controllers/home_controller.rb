c9fdec9bd5b9808d3370f95416796f474180c935
Andrew Breen <andrewbreen@crowdvenience.co>
2016-08-11 09:42:45 +1000

    Added live YT tiles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    Yt.configuration.api_key = "AIzaSyAclweov9YHqlmd4of52G0H4_3Cd40ieBY"
+    @playlist = Yt::Playlist.new url: 'youtube.com/playlist?list=PLbIc1971kgPCDE4kgkx6aIi47lldh_ugK'
   end
 end
