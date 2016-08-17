892a90a187e8193a22bfa6593f8f3b204c8bbb77
Andrew Breen <andrewbreen@crowdvenience.co>
2016-08-17 10:25:08 +1000

    Updated videos to show in reverse order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
     Yt.configuration.api_key = "AIzaSyAclweov9YHqlmd4of52G0H4_3Cd40ieBY"
-    @playlist = Yt::Playlist.new url: 'youtube.com/playlist?list=PL6MAHUvxQ94Z_EVuKAr-UTDkXLDBt78Tf'
+    _playlist = Yt::Playlist.new url: 'youtube.com/playlist?list=PL6MAHUvxQ94Z_EVuKAr-UTDkXLDBt78Tf'
+    @playlist = Array.new
+    _playlist.playlist_items.each do |item|
+      @playlist.push(item)
+    end
+    @playlist = @playlist.reverse
   end
 end
